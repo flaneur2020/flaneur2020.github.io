1ea482844c86e76181d1a540e58d7b9bdc893b5c
Li Yazhou <me.ssword@gmail.com>
2017-10-24 21:40:52 +0800

    pass predictOneVsAll.m

--- exercises/ml-andrew-ng/ex3/predictOneVsAll.m
+++ exercises/ml-andrew-ng/ex3/predictOneVsAll.m
@@ -28,10 +28,10 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
+%
 
 
-
+[r, p] = max(sigmoid(X * all_theta'), [], 2)
 
 
 
