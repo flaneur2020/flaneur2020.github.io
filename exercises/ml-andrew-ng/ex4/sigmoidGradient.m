07ae332abfad795d50a8063f0d5f8c97da6e9c9d
Li Yazhou <me.ssword@gmail.com>
2017-10-29 18:10:01 +0800

    pass sigmoid gradient

--- exercises/ml-andrew-ng/ex4/sigmoidGradient.m
+++ exercises/ml-andrew-ng/ex4/sigmoidGradient.m
@@ -6,13 +6,14 @@ function g = sigmoidGradient(z)
 %   vector. In particular, if z is a vector or matrix, you should return
 %   the gradient for each element.
 
-g = zeros(size(z));
+% g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-
+s = sigmoid(z)
+g = s .* (1 - s)
 
 
 
