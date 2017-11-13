32c493301880b7c41a1bf5302ae716e421817dd8
Li Yazhou <me.ssword@gmail.com>
2017-11-13 23:08:39 +0800

    pass Parameters (C, sigma) for Dataset 3

--- exercises/ml-andrew-ng/ex6/dataset3Params.m
+++ exercises/ml-andrew-ng/ex6/dataset3Params.m
@@ -9,8 +9,11 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
+end
+
+function [c, sigma] = findParams(X, y, Xval, yval)
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -22,6 +25,9 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+
+C = 1;
+sigma = 0.3;
 
 choices = [0.01 0.03 0.1 0.3 1 3 10 30];
 
