d8e1e9a8ca2f4e99d02b700471aa927edf239e2a
Li Yazhou <me.ssword@gmail.com>
2019-08-02 09:57:32 +0800

    add forth.rs

---
+++ exercises/rust/linkedlist/src/forth.rs
@@ -0,0 +1,46 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem: elem,
+            next: None,
+            prev: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new(elem: T) -> Self {
+        List { head: None, tail: None }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            None => {
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            },
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().prev = Some(old_head);
+                self.head = Some(new_head);
+            }
+        }
+    }
+}
