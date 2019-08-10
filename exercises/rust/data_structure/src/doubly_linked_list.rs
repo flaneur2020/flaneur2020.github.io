d4594eb1d0e75371fd1337bbf546b62fdc16b8d9
Li Yazhou <me.ssword@gmail.com>
2019-08-10 23:04:52 +0800

    make the doubly linked list works

--- exercises/rust/data_structure/src/doubly_linked_list.rs
+++ exercises/rust/data_structure/src/doubly_linked_list.rs
@@ -3,6 +3,8 @@
 
 use std::cell::RefCell;
 use std::rc::{Rc, Weak};
+use std::fmt::Display;
+
 
 struct Node<T> {
     data: T,
@@ -12,7 +14,7 @@ struct Node<T> {
 
 struct DoublyLinkedList<T> {
     first: Option<Rc<RefCell<Node<T>>>>,
-    last: Option<Weak<RefCell<Node<T>>>>,
+    last: Option<Rc<RefCell<Node<T>>>>,
 }
 
 impl<T> Node<T> {
@@ -25,22 +27,13 @@ impl<T> Node<T> {
     }
 
     fn append(node: &mut Rc<RefCell<Node<T>>>, data: T) -> Option<Rc<RefCell<Node<T>>>> {
-        let mut new_node = Node::new(data);
-        let is_last = node.borrow().next.is_none();
-        if is_last {
-            new_node.prev = Some(Rc::downgrade(node));
-            let new_rc = Rc::new(RefCell::new(new_node));
-            node.borrow_mut().next = Some(new_rc.clone());
-            Some(new_rc)
-        } else {
-            let old_next_link = node.borrow_mut().next.take();
-            new_node.prev = Some(Rc::downgrade(node));
-            new_node.next = old_next_link.clone();
-            let new_rc = Rc::new(RefCell::new(new_node));
-            node.borrow_mut().next = Some(new_rc.clone());
-            old_next_link.unwrap().borrow_mut().prev = Some(Rc::downgrade(&new_rc));
-            Some(new_rc)
-        }
+        let old_next_link = node.borrow_mut().next.take();
+        let new_node = Rc::new(RefCell::new(Node::new(data)));
+        new_node.borrow_mut().prev = Some(Rc::downgrade(node));
+        new_node.borrow_mut().next = old_next_link.clone();
+        node.borrow_mut().next = Some(new_node.clone());
+        old_next_link.map(|n| n.borrow_mut().prev = Some(Rc::downgrade(&new_node)));
+        Some(new_node)
     }
 }
 
@@ -48,4 +41,49 @@ impl<T> DoublyLinkedList<T> {
     pub fn new() -> Self {
         Self { first: None, last: None }
     }
+
+    pub fn append(&mut self, data: T) {
+        if self.last.is_none() {
+            let rc = Rc::new(RefCell::new(Node::new(data)));
+            self.first = Some(rc.clone());
+            self.last = Some(rc);
+        } else {
+            let mut last_node = self.last.take().unwrap();
+            self.last = Node::append(&mut last_node, data);
+            self.first = self.last.clone();
+        }
+    }
+}
+
+impl<T: Display> Display for DoublyLinkedList<T> {
+    fn fmt(&self, w: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+        write!(w, "[")?;
+        let mut node = self.first.clone();
+        while let Some(n) = node {
+            write!(w, "{}", n.borrow().data)?;
+            node = n.borrow().next.clone();
+            if node.is_some() {
+                write!(w, ", ")?;
+            }
+        }
+        write!(w, "]")
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_linked_list_append() {
+        // cargo test -- --nocapture
+        let mut l: DoublyLinkedList<u32> = DoublyLinkedList::new();
+        l.append(1);
+        l.append(2);
+        l.append(3);
+        println!("{}", l)
+    }
+
 }
