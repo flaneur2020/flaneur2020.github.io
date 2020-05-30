88bf4f9a3e1fe48a09d8bfe8a4330b4501dba4ff
Li Yazhou <liyazhou@fenbi.com>
2020-05-31 00:51:12 +0800

    add my-wasm-filter

--- exercises/rust/my-wasm-filter/src/lib.rs
+++ exercises/rust/my-wasm-filter/src/lib.rs
@@ -1,3 +1,30 @@
+use log::trace;
+use proxy_wasm::traits::{HttpContext, Context};
+use proxy_wasm::types::Action;
+
+struct SecretTokenFilter {
+    context_id: u32,
+}
+
+impl Context for SecretTokenFilter {}
+
+impl HttpContext for SecretTokenFilter {
+    fn on_http_request_headers(&mut self, _: usize) -> Action {
+        trace!("===== first!");
+
+        match self.get_http_request_header("X-TOKEN") {
+            Some(token) if token == "secret123" => {
+                self.resume_http_request();
+                Action::Continue
+            },
+            _ => {
+                self.send_http_response(403, vec![("X-SECRET-FILTER", "0.1.0")], Some(b"Access Forbidden."));
+                Action::Pause
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
