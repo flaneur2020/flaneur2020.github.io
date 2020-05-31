278862c3b91d0c51d389814bb7c1b980784f640b
Li Yazhou <liyazhou@fenbi.com>
2020-05-31 15:44:55 +0800

    wtf

--- hacks/rust/my-wasm-filter/src/lib.rs
+++ hacks/rust/my-wasm-filter/src/lib.rs
@@ -1,6 +1,15 @@
-use log::trace;
+use log;
 use proxy_wasm::traits::{HttpContext, Context};
-use proxy_wasm::types::Action;
+use proxy_wasm::types::{Action, LogLevel};
+
+#[no_mangle]
+pub fn _start() {
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_http_context(|context_id, _| -> Box<dyn HttpContext> {
+        Box::new(SecretTokenFilter { context_id })
+    });
+}
+
 
 struct SecretTokenFilter {
     context_id: u32,
@@ -10,7 +19,7 @@ impl Context for SecretTokenFilter {}
 
 impl HttpContext for SecretTokenFilter {
     fn on_http_request_headers(&mut self, _: usize) -> Action {
-        trace!("===== first!");
+        log::info!("===== first!");
 
         match self.get_http_request_header("X-TOKEN") {
             Some(token) if token == "secret123" => {
@@ -18,7 +27,7 @@ impl HttpContext for SecretTokenFilter {
                 Action::Continue
             },
             _ => {
-                self.send_http_response(403, vec![("X-SECRET-FILTER", "0.1.0")], Some(b"Access Forbidden."));
+                self.send_http_response(403, vec![("X-SECRET-FILTER", "0.1.0")], Some(b"Access Forbidden..."));
                 Action::Pause
             }
         }
