833d9f0350050a3926307a23ee701e017e3a9ffd
Li Yazhou <me.ssword@gmail.com>
2023-06-24 22:44:05 +0800

    add softmax

--- exercises/nn/nn.py
+++ exercises/nn/nn.py
@@ -18,3 +18,15 @@ class Affine:
         self.dW = self.x.T.dot(dout)
         self.db = np.sum(dout, axis=0)
         return dx
+    
+
+class Softmax:
+    def __init__(self):
+        pass
+
+    def forward(self, a):
+        exp_a = np.exp(a)
+        return exp_a / np.sum(exp_a)
+
+    def backward(self, dout):
+        raise Exception("not implemented")
