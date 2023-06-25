175954314e6472c0ba512673df8409745cdc9e97
Li Yazhou <me.ssword@gmail.com>
2023-06-25 22:05:06 +0800

    add test about softmax

--- exercises/nn/tests/nn_test.py
+++ exercises/nn/tests/nn_test.py
@@ -1,7 +1,15 @@
 import unittest
+import numpy as np
+from nn import Softmax
+
 
 class TestLayer(unittest.TestCase):
-    pass
+    def test_softmax(self):
+        softmax = Softmax()
+        r = softmax.forward(np.array([0.3, 2.9, 4.0]))
+        self.assertAlmostEqual(r[0], 0.018211273)
+        self.assertAlmostEqual(r[1], 0.2451918129)
+        self.assertAlmostEqual(r[2], 0.7365969138)
 
 
 if __name__ == '__main__':
