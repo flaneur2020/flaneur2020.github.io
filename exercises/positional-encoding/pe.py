842edc3926f23e61bc79d7342900513bbf5035a9
Li Yazhou <me.ssword@gmail.com>
2023-08-03 10:47:55 +0800

    sync

---
+++ exercises/positional-encoding/pe.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def getPositionEncoding(seq_len, d, n=10000):
+    P = np.zeros((seq_len, d))
+    for k in range(seq_len):
+        for i in np.arange(int(d/2)):
+            denominator = np.power(n, 2*i/d)
+            P[k, 2*i] = np.sin(k/denominator)
+            P[k, 2*i+1] = np.cos(k/denominator)
+    return P
+
+
+pos = 2
+dim = 512
+
+P = getPositionEncoding(seq_len=1024, d=dim, n=1000)
+
+_, axes = plt.subplots(nrows=8, ncols=2)
+axes = axes.flatten()
+
+for i in range(8):
+    pos = 2 ** i
+    axes[i].plot(np.arange(0, dim), P[pos, :], '.', markersize=1)
+    # axes[pos].title('pos = ' + str(pos))
+
+plt.pause(3600)
--- obsidian/Documents/2023-08/Transformer Architecture：The Positional Encoding.md
+++ obsidian/Documents/2023-08/Transformer Architecture：The Positional Encoding.md
@@ -17,4 +17,6 @@ Ideally, the following criteria should be satisfied:
 
 如果希望通过二进制来表示位置，会相当于上图的样子。
 
+<mark>LSB 的 bit 会在每底层一个数字时变化一次，右边第二个 bit 会在每两个数字递增时变化一次，以此递推</mark>。
+
 但是在 float 世界中，使用上面的 binary 表示会浪费空间。因此，可以使用它的 float 替代品：Sinusoidal 函数。
