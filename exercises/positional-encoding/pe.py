2aef2e86ff60290428ec6822d245bbff5e1b777f
Li Yazhou <me.ssword@gmail.com>
2023-08-05 11:28:00 +0800

    add note about pe.py

--- exercises/positional-encoding/pe.py
+++ exercises/positional-encoding/pe.py
@@ -11,17 +11,36 @@ def getPositionEncoding(seq_len, d, n=10000):
     return P
 
 
-pos = 2
-dim = 512
+def plotSinArgs(dim=20, n=10000):
+    pos = np.arange(0, 100)
+    y = [1/np.power(n, pos/dim)]
+    plt.plot(pos, y, '.')
+    plt.grid(True, which='both')
+    plt.pause(3000)
 
-P = getPositionEncoding(seq_len=1024, d=dim, n=1000)
 
-_, axes = plt.subplots(nrows=8, ncols=2)
-axes = axes.flatten()
+def plotSin():
+    x = np.arange(0, 10 * 2 * np.pi, 0.01)
+    y = np.sin(x)
+    plt.plot(x, y)
+    plt.grid(True, which='both')
+    plt.pause(3000)
 
-for i in range(8):
-    pos = 2 ** i
-    axes[i].plot(np.arange(0, dim), P[pos, :], '.', markersize=1)
-    # axes[pos].title('pos = ' + str(pos))
+plotSinArgs()
 
-plt.pause(3600)
+# plotSinArgs()
+
+
+def plotSpe():
+    pos = 2
+    dim = 512
+    P = getPositionEncoding(seq_len=1024, d=dim, n=1000)
+    _, axes = plt.subplots(nrows=8, ncols=2)
+    axes = axes.flatten()
+
+    for i in range(8):
+        pos = 2 ** i
+        axes[i].plot(np.arange(0, dim), P[pos, :], '.', markersize=1)
+        # axes[pos].title('pos = ' + str(pos))
+
+    plt.pause(3600)
