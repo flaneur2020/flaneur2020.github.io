1f710f2422183ed1bf73042e4e1d2783f9d7f2d4
Li Yazhou <me.ssword@gmail.com>
2023-10-21 10:55:02 +0800

    init_gpu

--- exercises/rust/webgpu/src/main.rs
+++ exercises/rust/webgpu/src/main.rs
@@ -1,3 +1,39 @@
+struct App {
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+}
+
+impl App {
+    async fn new() -> Self {
+        let (device, queue) = init_gpu().await;
+        Self { device, queue }
+    }
+}
+
+async fn init_gpu() -> (wgpu::Device, wgpu::Queue) {
+    // Instantiates instance of WebGPU
+    let instance = wgpu::Instance::default();
+    // `request_adapter` instantiates the general connection to the GPU
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions::default())
+        .await
+        .unwrap();
+    // `request_device` instantiates the feature specific connection to the GPU, defining some parameters,
+    //  `features` being the available features.
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::downlevel_defaults(),
+            },
+            None,
+        )
+        .await
+        .unwrap();
+    (device, queue)
+}
+
 fn main() {
     println!("Hello, world!");
 }
