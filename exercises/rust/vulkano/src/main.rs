db5498cb0a7f1b8ae38f320a81a163b4e7859355
Li Yazhou <me.ssword@gmail.com>
2024-05-05 21:39:01 +0800

    refactor

--- exercises/rust/vulkano/src/main.rs
+++ exercises/rust/vulkano/src/main.rs
@@ -20,14 +20,17 @@ use vulkano::{
         allocator::StandardCommandBufferAllocator, AutoCommandBufferBuilder, CommandBufferUsage,
     },
     descriptor_set::{
-        allocator::StandardDescriptorSetAllocator, PersistentDescriptorSet, WriteDescriptorSet,
+        allocator::{DescriptorSetAllocator, StandardDescriptorSetAllocator},
+        PersistentDescriptorSet, WriteDescriptorSet,
     },
     device::{
-        physical::PhysicalDeviceType, Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo,
-        QueueFlags,
+        physical::PhysicalDeviceType, Device, DeviceCreateInfo, DeviceExtensions, Queue,
+        QueueCreateInfo, QueueFlags,
     },
     instance::{Instance, InstanceCreateFlags, InstanceCreateInfo},
-    memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator},
+    memory::allocator::{
+        AllocationCreateInfo, MemoryAllocator, MemoryTypeFilter, StandardMemoryAllocator,
+    },
     pipeline::{
         compute::ComputePipelineCreateInfo, layout::PipelineDescriptorSetLayoutCreateInfo,
         ComputePipeline, Pipeline, PipelineBindPoint, PipelineLayout,
@@ -37,83 +40,106 @@ use vulkano::{
     VulkanLibrary,
 };
 
-fn main() {
-    // As with other examples, the first step is to create an instance.
-    let library = VulkanLibrary::new().unwrap();
-    let instance = Instance::new(
-        library,
-        InstanceCreateInfo {
-            flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
-            ..Default::default()
-        },
-    )
-    .unwrap();
+struct VkCompute {
+    device: Arc<Device>,
+    queue: Arc<Queue>,
+    memory_allocator: Arc<StandardMemoryAllocator>,
+    descriptor_set_allocator: Arc<StandardDescriptorSetAllocator>,
+    command_buffer_allocator: Arc<StandardCommandBufferAllocator>,
+}
 
-    // Choose which physical device to use.
-    let device_extensions = DeviceExtensions {
-        khr_storage_buffer_storage_class: true,
-        ..DeviceExtensions::empty()
-    };
-    let (physical_device, queue_family_index) = instance
-        .enumerate_physical_devices()
-        .unwrap()
-        .filter(|p| p.supported_extensions().contains(&device_extensions))
-        .filter_map(|p| {
-            // The Vulkan specs guarantee that a compliant implementation must provide at least one
-            // queue that supports compute operations.
-            p.queue_family_properties()
-                .iter()
-                .position(|q| q.queue_flags.intersects(QueueFlags::COMPUTE))
-                .map(|i| (p, i as u32))
-        })
-        .min_by_key(|(p, _)| match p.properties().device_type {
-            PhysicalDeviceType::DiscreteGpu => 0,
-            PhysicalDeviceType::IntegratedGpu => 1,
-            PhysicalDeviceType::VirtualGpu => 2,
-            PhysicalDeviceType::Cpu => 3,
-            PhysicalDeviceType::Other => 4,
-            _ => 5,
-        })
+impl VkCompute {
+    fn new() -> Self {
+        let (device, queue) = Self::init_device();
+
+        let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+        let descriptor_set_allocator = Arc::new(StandardDescriptorSetAllocator::new(
+            device.clone(),
+            Default::default(),
+        ));
+        let command_buffer_allocator = Arc::new(StandardCommandBufferAllocator::new(
+            device.clone(),
+            Default::default(),
+        ));
+
+        Self {
+            device,
+            queue,
+            memory_allocator,
+            descriptor_set_allocator,
+            command_buffer_allocator,
+        }
+    }
+
+    fn init_device() -> (Arc<Device>, Arc<Queue>) {
+        // As with other examples, the first step is to create an instance.
+        let library = VulkanLibrary::new().unwrap();
+        let instance = Instance::new(
+            library,
+            InstanceCreateInfo {
+                flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+                ..Default::default()
+            },
+        )
         .unwrap();
 
-    println!(
-        "Using device: {} (type: {:?})",
-        physical_device.properties().device_name,
-        physical_device.properties().device_type,
-    );
+        // Choose which physical device to use.
+        let device_extensions = DeviceExtensions {
+            khr_storage_buffer_storage_class: true,
+            ..DeviceExtensions::empty()
+        };
+        let (physical_device, queue_family_index) = instance
+            .enumerate_physical_devices()
+            .unwrap()
+            .filter(|p| p.supported_extensions().contains(&device_extensions))
+            .filter_map(|p| {
+                // The Vulkan specs guarantee that a compliant implementation must provide at least one
+                // queue that supports compute operations.
+                p.queue_family_properties()
+                    .iter()
+                    .position(|q| q.queue_flags.intersects(QueueFlags::COMPUTE))
+                    .map(|i| (p, i as u32))
+            })
+            .min_by_key(|(p, _)| match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                PhysicalDeviceType::Other => 4,
+                _ => 5,
+            })
+            .unwrap();
 
-    // Now initializing the device.
-    let (device, mut queues) = Device::new(
-        physical_device,
-        DeviceCreateInfo {
-            enabled_extensions: device_extensions,
-            queue_create_infos: vec![QueueCreateInfo {
-                queue_family_index,
+        println!(
+            "Using device: {} (type: {:?})",
+            physical_device.properties().device_name,
+            physical_device.properties().device_type,
+        );
+
+        // Now initializing the device.
+        let (device, mut queues) = Device::new(
+            physical_device,
+            DeviceCreateInfo {
+                enabled_extensions: device_extensions,
+                queue_create_infos: vec![QueueCreateInfo {
+                    queue_family_index,
+                    ..Default::default()
+                }],
                 ..Default::default()
-            }],
-            ..Default::default()
-        },
-    )
-    .unwrap();
+            },
+        )
+        .unwrap();
 
-    // Since we can request multiple queues, the `queues` variable is in fact an iterator. In this
-    // example we use only one queue, so we just retrieve the first and only element of the
-    // iterator and throw it away.
-    let queue = queues.next().unwrap();
+        // Since we can request multiple queues, the `queues` variable is in fact an iterator. In this
+        // example we use only one queue, so we just retrieve the first and only element of the
+        // iterator and throw it away.
+        let queue = queues.next().unwrap();
+        (device, queue)
+    }
+}
 
-    // Now let's get to the actual example.
-    //
-    // What we are going to do is very basic: we are going to fill a buffer with 64k integers and
-    // ask the GPU to multiply each of them by 12.
-    //
-    // GPUs are very good at parallel computations (SIMD-like operations), and thus will do this
-    // much more quickly than a CPU would do. While a CPU would typically multiply them one by one
-    // or four by four, a GPU will do it by groups of 32 or 64.
-    //
-    // Note however that in a real-life situation for such a simple operation the cost of accessing
-    // memory usually outweighs the benefits of a faster calculation. Since both the CPU and the
-    // GPU will need to access data, there is no other choice but to transfer the data through the
-    // slow PCI express bus.
+fn main() {
+    let compute = VkCompute::new();
 
     // We need to create the compute pipeline that describes our operation.
     //
@@ -139,35 +165,29 @@ fn main() {
                 ",
             }
         }
-        let cs = cs::load(device.clone())
+        let cs = cs::load(compute.device.clone())
             .unwrap()
             .entry_point("main")
             .unwrap();
         let stage = PipelineShaderStageCreateInfo::new(cs);
         let layout = PipelineLayout::new(
-            device.clone(),
+            compute.device.clone(),
             PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
-                .into_pipeline_layout_create_info(device.clone())
+                .into_pipeline_layout_create_info(compute.device.clone())
                 .unwrap(),
         )
         .unwrap();
         ComputePipeline::new(
-            device.clone(),
+            compute.device.clone(),
             None,
             ComputePipelineCreateInfo::stage_layout(stage, layout),
         )
         .unwrap()
     };
 
-    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
-    let descriptor_set_allocator =
-        StandardDescriptorSetAllocator::new(device.clone(), Default::default());
-    let command_buffer_allocator =
-        StandardCommandBufferAllocator::new(device.clone(), Default::default());
-
     // We start by creating the buffer that will store the data.
     let data_buffer = Buffer::from_iter(
-        memory_allocator,
+        compute.memory_allocator,
         BufferCreateInfo {
             usage: BufferUsage::STORAGE_BUFFER,
             ..Default::default()
@@ -192,7 +212,7 @@ fn main() {
     // descriptor sets that each contain the buffer you want to run the shader on.
     let layout = pipeline.layout().set_layouts().get(0).unwrap();
     let set = PersistentDescriptorSet::new(
-        &descriptor_set_allocator,
+        &compute.descriptor_set_allocator,
         layout.clone(),
         [WriteDescriptorSet::buffer(0, data_buffer.clone())],
         [],
@@ -201,8 +221,8 @@ fn main() {
 
     // In order to execute our operation, we have to build a command buffer.
     let mut builder = AutoCommandBufferBuilder::primary(
-        &command_buffer_allocator,
-        queue.queue_family_index(),
+        &compute.command_buffer_allocator,
+        compute.queue.queue_family_index(),
         CommandBufferUsage::OneTimeSubmit,
     )
     .unwrap();
@@ -230,8 +250,8 @@ fn main() {
     let command_buffer = builder.build().unwrap();
 
     // Let's execute this command buffer now.
-    let future = sync::now(device)
-        .then_execute(queue, command_buffer)
+    let future = sync::now(compute.device)
+        .then_execute(compute.queue, command_buffer)
         .unwrap()
         // This line instructs the GPU to signal a *fence* once the command buffer has finished
         // execution. A fence is a Vulkan object that allows the CPU to know when the GPU has
