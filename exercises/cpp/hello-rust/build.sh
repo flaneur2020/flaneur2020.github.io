a4b14acf8fc9e74b5f169fc1051b92b79bf97043
Li Yazhou <me.ssword@gmail.com>
2025-04-11 18:21:05 +0800

    fix os x build

--- exercises/cpp/hello-rust/build.sh
+++ exercises/cpp/hello-rust/build.sh
@@ -1,2 +1,7 @@
 cd rustlib
 cargo build
+
+cd ..
+mkdir build
+cmake -B build -S .
+cmake --build build
--- exercises/cpp/hello-rust/rustlib/build.rs
+++ exercises/cpp/hello-rust/rustlib/build.rs
@@ -1,7 +1,10 @@
 fn main() {
-    cxx_build::bridge("src/lib.rs")
-        .flag_if_supported("-std=c++14")
-        .compile("rustlib");
+    let mut build = cxx_build::bridge("src/lib.rs");
+
+    #[cfg(target_os = "macos")]
+    build.flag("-std=c++14");
+
+    build.compile("rustlib");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
 }
